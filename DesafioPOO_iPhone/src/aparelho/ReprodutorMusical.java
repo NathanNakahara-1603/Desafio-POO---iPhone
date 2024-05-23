464be71350390b9c7bf82a249c706d48049df1b2
NathanNakahara-1603 <nathannakahara@gmail.com>
2024-05-23 10:08:53 -0300

    Commit inicial

---
+++ DesafioPOO_iPhone/src/aparelho/ReprodutorMusical.java
@@ -0,0 +1,10 @@
+package aparelho;
+
+public interface ReprodutorMusical {
+
+    public void tocar();
+
+    public void pausar();
+
+    public void selecionarMusica(String Musica);
+}
---
+++ DesafioPOO_iPhone/src/principal/iPhone.java
@@ -0,0 +1,55 @@
+package principal;
+
+import aparelho.AparelhoTelefonico;
+import aparelho.NavegadorInternet;
+import aparelho.ReprodutorMusical;
+
+
+public class iPhone implements ReprodutorMusical,AparelhoTelefonico,NavegadorInternet{
+
+    @Override
+    public void tocar() {
+        
+    }
+
+    @Override
+    public void pausar() {
+        
+    }
+
+    @Override
+    public void atender() {
+       
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void selecionarMusica(String Musica) {
+
+    }
+    
+}
